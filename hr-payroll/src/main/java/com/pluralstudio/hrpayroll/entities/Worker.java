088ff1850cefdff31a9e1e0a5c0ee67ae8ec371e
Allan Kleber Maranhao <allan.maranhao@CTNDELLHT6HJF3.pampa.compasso>
2021-07-14 14:18:07 -0300

    RestTemplate

--- hr-payroll/src/main/java/com/pluralstudio/hrpayroll/entities/Worker.java
+++ hr-payroll/src/main/java/com/pluralstudio/hrpayroll/entities/Worker.java
@@ -1,28 +1,21 @@
-package com.pluralstudio.hrworker.entities;
+package com.pluralstudio.hrpayroll.entities;
 
 import java.io.Serializable;
 import java.util.Objects;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
 
-@Entity
-@Table(name = "tb_worker")
+
 public class Worker implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
 	
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	
 	private Long id;
 	private String name;
 	private Double dailyIncome;
 	
 	public Worker() {
-		
+
 	}
 	
 	public Worker(Long id, String name, Double dailyIncome) {
