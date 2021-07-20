d48ac99c115386d43debe53ed23a2ecb2aff1e23
Allan Kleber Maranhao <allan.maranhao@CTNDELLHT6HJF3.pampa.compasso>
2021-07-20 15:53:28 -0300

    Project hr-user created

---
+++ hr-user/src/test/java/com/pluralstudio/hruser/HrUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pluralstudio.hruser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
