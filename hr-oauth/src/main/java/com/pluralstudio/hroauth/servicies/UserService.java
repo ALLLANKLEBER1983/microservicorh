b00fcf02f7bb7b34e5bdf08a3984e6df8ea6ffe3
ALLANKLEBER1983 <allankleber1983@gmail.com>
2021-07-27 22:10:39 -0300

    Loggin,JWT token

--- hr-oauth/src/main/java/com/pluralstudio/hroauth/servicies/UserService.java
+++ hr-oauth/src/main/java/com/pluralstudio/hroauth/servicies/UserService.java
@@ -3,10 +3,13 @@ package com.pluralstudio.hroauth.servicies;
 import com.pluralstudio.hroauth.entities.User;
 import com.pluralstudio.hroauth.feignclients.UserFeignClient;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService{
 
     @Autowired
     private UserFeignClient userFeignClient;
@@ -18,6 +21,15 @@ public class UserService {
         }
         return user;
     }
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = userFeignClient.findByEmail(username).getBody();
+        if(user == null){
+            throw new UsernameNotFoundException("Email not found");
+        }
+        return user;
+	}
 
 
 }
