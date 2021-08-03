2a1bebf7ae42420bda4c39b518a482b7d667e547
Allan Kleber Maranhao <allan.maranhao@CTNDELLHT6HJF3.pampa.compasso>
2021-08-03 10:12:57 -0300

    cors configuration

--- hr-oauth/src/main/java/com/pluralstudio/hroauth/config/SecurityConfig.java
+++ hr-oauth/src/main/java/com/pluralstudio/hroauth/config/SecurityConfig.java
@@ -5,9 +5,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import javax.ws.rs.HttpMethod;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -28,6 +31,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Autowired
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder);
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().antMatchers(HttpMethod.GET, "/users/search/**").permitAll();
 	}
 	
 	
